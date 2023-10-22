c05a5c8375a6ac53fb94ac64a07c2b9f415b279e
Amir <106912785+am1r0d@users.noreply.github.com>
2023-10-22 04:06:14 +0330

    add filter button and create MenuItem component

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { SafeAreaView, Text, View, TextInput, ScrollView } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import { Pressable, Text, View, TextInput, ScrollView } from "react-native";
 import Carousel from "../components/Carousel";
 import FoodTypes from "../components/FoodTypes";
 import QuickFood from "../components/QuickFood";
+import hotels from "../data/hotels";
+import MenuItem from "../components/MenuItem";
+
+// Icons
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
+    const data = hotels;
     return (
         <ScrollView style={{ marginTop: 50 }}>
             {/* Search Bar */}
@@ -36,6 +42,65 @@ const HomeScreen = () => {
 
             {/* Quick Food Components */}
             <QuickFood />
+
+            {/* Filter buttons */}
+            <View
+                style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                }}
+            >
+                <Pressable
+                    style={{
+                        marginHorizontal: 10,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        borderWidth: 1,
+                        borderColor: "#D0D0D0",
+                        padding: 10,
+                        borderRadius: 20,
+                        justifyContent: "center",
+                        width: 120,
+                    }}
+                >
+                    <Text style={{ marginRight: 6 }}>Filter</Text>
+                    <Ionicons name="filter" size={20} color="black" />
+                </Pressable>
+                <Pressable
+                    style={{
+                        marginHorizontal: 10,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        borderWidth: 1,
+                        borderColor: "#D0D0D0",
+                        padding: 10,
+                        borderRadius: 20,
+                        justifyContent: "center",
+                        width: 120,
+                    }}
+                >
+                    <Text>Sort by rating</Text>
+                </Pressable>
+                <Pressable
+                    style={{
+                        marginHorizontal: 10,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        borderWidth: 1,
+                        borderColor: "#D0D0D0",
+                        padding: 10,
+                        borderRadius: 20,
+                        justifyContent: "center",
+                    }}
+                >
+                    <Text>Sort by price</Text>
+                </Pressable>
+            </View>
+
+            {data.map((item, index) => (
+                <MenuItem key={index} item={item} />
+            ))}
         </ScrollView>
     );
 };
